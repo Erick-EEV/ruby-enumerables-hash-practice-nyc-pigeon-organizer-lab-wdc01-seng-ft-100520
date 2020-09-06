e2c481fac8aa6ad6800f54ef34fdae2fc58824c6
Erick Villeda <eeverickeev@gmail.com>
2020-09-06 15:43:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array| 
     binding.pry
     final_array
-    value.each 
+    value.each do |name_key|
+    end
   end
   binding.pry
 end
