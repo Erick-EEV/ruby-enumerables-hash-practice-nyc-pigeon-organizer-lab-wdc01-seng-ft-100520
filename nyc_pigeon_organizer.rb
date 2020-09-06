598d76c22ae89e91ef9f0da3a697925d588a1772
Erick Villeda <eeverickeev@gmail.com>
2020-09-06 15:45:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array| 
-    binding.pry
-    final_array
+  final_array
     value.each do |name_key, name_array|
     end
   end
