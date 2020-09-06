3e9c85f712768867ff3d826b54d45a748b7954a4
Erick Villeda <eeverickeev@gmail.com>
2020-09-06 16:07:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,9 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(first_key, value), final_array| 
   final_array
     value.each do |second_key, names_array|
-      names_array.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+      names_array.each do |names_array|
+        if !final_array[names_array]
+          final_array[names_array] = {}
   end
       if !final_array[names_array][first_key]
         !final_array[names_array][first_key] = []
