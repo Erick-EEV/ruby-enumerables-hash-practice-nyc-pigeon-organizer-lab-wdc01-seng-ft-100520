9fe285be554a2240dfb135d46aa9ef1ee073e737
Erick Villeda <eeverickeev@gmail.com>
2020-09-06 15:20:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_data = {
-    :names => ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"],
-      :color => ["purple", "grey", "white", "brown"],
-      :gender => ["male", "female"],
-      :lives => ["Subway", "Central Park", "Library", "City Hall"],
-      :idont => [],
-      :knowwhat => [],
-      :imdoing => []
+ :
   }
 end
